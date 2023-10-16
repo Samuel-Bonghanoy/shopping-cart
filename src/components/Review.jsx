681e6d7b981c33389e85b2240f8c78291617e952
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-16 20:38:48 +0800

    feat: added header component

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -4,7 +4,7 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import { useCart } from "../contexts/CartContext";
-import { Button } from "@mui/material";
+import { Autocomplete, Button, TextField } from "@mui/material";
 
 export default function Review() {
   const { cart } = useCart();
@@ -30,7 +30,38 @@ export default function Review() {
             PHP {String(price)}
           </Typography>
         </ListItem>
+        <Typography
+          variant="subtitle1"
+          sx={{ fontWeight: 700, marginBottom: "10px" }}
+        >
+          Shipping Address
+        </Typography>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={[
+            { label: "Standard Delivery - PHP 100", id: 1 },
+            { label: "Fast Delivery - PHP 300", id: 2 },
+          ]}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Movie" />}
+        />
+        <Typography
+          variant="subtitle1"
+          sx={{ fontWeight: 700, marginBottom: "10px", marginTop: "15px" }}
+        >
+          Promo Code
+        </Typography>
+        <TextField
+          fullWidth
+          id="filled-basic"
+          label="Enter your code"
+          variant="filled"
+        />
         <div className="flex flex-col gap-3 justify-between mt-5">
+          <Button color="success" fullWidth variant="contained">
+            Apply coupon
+          </Button>
           <Button fullWidth variant="contained">
             Place Order
           </Button>
